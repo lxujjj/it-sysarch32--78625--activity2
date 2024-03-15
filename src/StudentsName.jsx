5c95c3ea38861375821e65b8a8156e39fb03af3b
Jelu Mari Cajipe <jjelumari@gmail.com>
2024-03-16 01:48:37 +0800

    LORDDDDDDDDDD

--- src/StudentsName.jsx
+++ src/StudentsName.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 const studentsData = [
     
         { pic: "https://i.ibb.co/B4pKckG/432733711-2312354068961572-5913265791537854259-n.jpg", name: "Jelu Mari Cajipe", email: "jjelumar@gmail.com" },
-        { pic: "https://i.ibb.co/YBSkWvT/gheyl.png", name: "Gheyl Aguilar", email: "jjelumar@gmail.com" },
+        { pic: "https://i.ibb.co/YBSkWvT/gheyl.png", name: "Gheyl R. Aguilar", email: "jjelumar@gmail.com" },
         { pic: "https://i.ibb.co/YhKgfxQ/432358124-914593556983933-1625678965418093293-n.jpg", name: "Loui Canete", email: "jjelumar@gmail.com" },
         { pic: "https://i.ibb.co/FhCL4GT/188239030-1247549975660861-8707542890700673845-n.jpg",name: "Louie Jay Jugan", email: "jjelumar@gmail.com" },
         { pic: "https://i.ibb.co/pdCS5Wq/415314778-1377061689576237-8878018506239282928-n.jpg",name: "Angel  Medez", email: "jjelumar@gmail.com" },
