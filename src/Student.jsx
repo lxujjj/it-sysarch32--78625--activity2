802c14697a1db9e508ef43e69cace2bcdf37a47f
Jelu Mari Cajipe <jjelumari@gmail.com>
2024-03-16 01:23:51 +0800

    PLEASE LORD

--- src/Student.jsx
+++ src/Student.jsx
@@ -1,23 +1,13 @@
-function Student(){
+import React from "react";
 
-    return(
-        <div class="row">
-  <div class="column">
-    <div class="card">
-    <img src="https://media.allure.com/photos/65ae89310307a111c5cf40df/4:3/w_2404,h_1803,c_limit/jlo%202024.jpg"alt="Avatar" />
-      <h3>Jelu Mari Cajipe</h3>
-      <p>Actress</p>
+function Student({ pic, name, email }) {
+  return (
+    <div className="card">
+      <img src={pic} alt="profile" />
+      <h2 id="name">{name}</h2>
+      <p id="email">{email}</p>
     </div>
-  </div>
-  <div class="column">
-    <div class="card">
-    <img src="https://media.allure.com/photos/65ae89310307a111c5cf40df/4:3/w_2404,h_1803,c_limit/jlo%202024.jpg"alt="Avatar"/>
-      <h3>Jelu Mari Cajipe</h3>
-      <p>Actress</p>
-    </div>
-  </div>
-  </div>
-  
-    );
+  );
 }
-export default Student
+
+export default Student;
